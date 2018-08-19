38eb27b2ff70833c21c666aef9dfd71792936478
ayn <vincentayndecastro@gmail.com>
2018-08-19 18:55:23 -0500

    basic

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+/*
+1. make an array of fighters
+2. give them attack, counter and HP
+3. make the fighter's attack increases by the base attack everytime they attack
+4. counter attack stays the same
+5. first fighter chosen goes to the hero area
+6. second fighter chosen goes to the villain
+*/
+
